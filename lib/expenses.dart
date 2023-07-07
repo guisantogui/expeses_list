1c65358be2a96e667d56f7ba8278ab02900ca20b
makadu <makaduapp@outlook.com>
2023-07-07 07:20:12 -0300

    Projeto de listagem de despesas

---
+++ lib/expenses.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:flutter/material.dart';
+import './models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        amount: 15,
+        title: "Flutter Course",
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        amount: 55,
+        title: "Meat",
+        date: DateTime.now(),
+        category: Category.food),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        const Text("CHART"),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        ),
+      ],
+    ));
+  }
+}
---
+++ lib/expenses_list.dart
@@ -0,0 +1,16 @@
+import './models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  final List<Expense> expenses;
+
+  const ExpensesList({super.key, required this.expenses});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
