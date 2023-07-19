0b52b4c070ab829e3a92b6d9cea2987028c2d181
makadu <makaduapp@outlook.com>
2023-07-19 19:25:31 -0300

    Customizando o tema do app

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  var kColorScheme =
+      ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 66, 141, 90));
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondary,
+          shadowColor: kColorScheme.inversePrimary,
+          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.tertiary,
+            textStyle: const TextStyle(color: Colors.white),
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontWeight: FontWeight.normal,
+                //color: Colors.white,
+              ),
+            ),
+      ),
       home: const Expenses(),
     );
   }
